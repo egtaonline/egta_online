d54109ea6a0056f26d78609cd8d2e0298f3b074a
Ben-Alexander Cassell <bcassell@umich.edu>
2011-02-08 16:11:13 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :rvm_ruby_string, '1.9.2'
 
 set :application, "EGTMAS Web Interface"
 set :repository,  "git@github.com:bcassell/EGTMAS-Web-Interface.git"
-
+set :branch, "mongoid"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
