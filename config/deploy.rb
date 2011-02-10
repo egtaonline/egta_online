67b29f7315f603b588c161adc2e20aae46de97e7
Ben-Alexander Cassell <bcassell@umich.edu>
2011-02-10 13:11:14 -0500

    jruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,25 +36,19 @@ namespace :daemons do
 
   desc "Start Daemons"
   task :start, :roles => :app do
-    run "rvm 1.9.2"
-    run "#{current_release}/lib/daemons/periodic_daemon_ctl start"
-    run "#{current_release}/lib/daemons/stalker_daemon.rb start"
-    run "rvm jruby-1.6.0.RC2"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/periodic_daemon_ctl start"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/stalker_daemon.rb start"
   end
 
   desc "Stop Daemons"
   task :stop, :roles => :app do
-    run "rvm 1.9.2"
-    run "#{current_release}/lib/daemons/periodic_daemon_ctl stop"
-    run "#{current_release}/lib/daemons/stalker_daemon.rb stop"
-    run "rvm jruby-1.6.0.RC2"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/periodic_daemon_ctl stop"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/stalker_daemon.rb stop"
   end
 
   desc "Restart Daemons"
   task :restart, :roles => :app do
-    run "rvm 1.9.2"
-    run "#{current_release}/lib/daemons/periodic_daemon_ctl restart"
-    run "#{current_release}/lib/daemons/stalker_daemon.rb restart"
-    run "rvm jruby-1.6.0.RC2"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/periodic_daemon_ctl restart"
+    run "rvm 1.9.2 #{current_release}/lib/daemons/stalker_daemon.rb restart"
   end
 end
