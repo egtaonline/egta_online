4c5640eed51359e037cf3edbc90aa2f6c2a5223b
Ben-Alexander Cassell <bcassell@umich.edu>
2011-02-13 16:48:03 -0500

    speedup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   before_filter :checkup
 
   def checkup
-    @sample_count = 0
-    Simulation.complete.each {|x| @sample_count += x.samples.count}
-    @active_simulation_count = Simulation.active.count
-    @complete_simulation_count = Simulation.complete.count
-    @active_scheduler_count = GameScheduler.active.count + ProfileScheduler.active.count + DeviationScheduler.active.count
-    @scheduler_count = GameScheduler.count + ProfileScheduler.count + DeviationScheduler.count
+    # @sample_count = 0
+    # Simulation.complete.each {|x| @sample_count += x.samples.count}
+    # @active_simulation_count = Simulation.active.count
+    # @complete_simulation_count = Simulation.complete.count
+    # @active_scheduler_count = GameScheduler.active.count + ProfileScheduler.active.count + DeviationScheduler.active.count
+    # @scheduler_count = GameScheduler.count + ProfileScheduler.count + DeviationScheduler.count
   end
 end
