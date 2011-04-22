470e3a8ea1d3cde10d010b1665e18befa925a23f
Ben-Alexander Cassell <bcassell@umich.edu>
2011-04-22 17:19:16 -0400

    post-removal

---
+++ app/models/scheduler.rb
@@ -0,0 +1,21 @@
+class Scheduler
+  include Mongoid::Document
+
+  has_many :simulations
+  field :process_memory, :type => Integer
+  field :time_per_sample, :type => Integer
+  field :jobs_per_request, :type => Integer
+
+  validates_presence_of :process_memory
+  validates_numericality_of :process_memory, :only_integer => true
+
+  validates_presence_of :time_per_sample
+  validates_numericality_of :time_per_sample, :only_integer => true
+
+  validates_presence_of :jobs_per_request
+  validates_numericality_of :jobs_per_request, :only_integer => true
+
+  def name
+    "#{self.time_per_sample}-#{self.process_memory}-#{self.qos}"
+  end
+end
