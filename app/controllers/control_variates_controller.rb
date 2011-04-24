515b79d1671a5a0c4ff1f2dd897f4c70050cf23b
Ben-Alexander Cassell <bcassell@umich.edu>
2011-04-24 16:16:31 -0400

    cv

--- app/controllers/control_variates_controller.rb
+++ app/controllers/control_variates_controller.rb
@@ -1,7 +1,6 @@
 #Manages the CRUD of ControlVariates
 class ControlVariatesController < GameDescendentsController
   before_filter :create_new_cv, :only => [:new, :add_feature, :remove_feature]
-  after_filter :calculate_feature_options, :only => [:new, :add_feature, :remove_feature]
 
   def show
     @control_variate = @game.control_variates.find(params[:id])
@@ -9,6 +8,7 @@ class ControlVariatesController < GameDescendentsController
 
   def new
     @cv_features = []
+    calculate_feature_options
   end
 
   def create
@@ -22,6 +22,7 @@ class ControlVariatesController < GameDescendentsController
   def add_feature
     params[:feature_names] ||= Array.new
     @cv_features = @game.calculate_cv_features(params)
+    calculate_feature_options
     respond_to do |format|
       format.js
     end
@@ -29,6 +30,7 @@ class ControlVariatesController < GameDescendentsController
 
   def remove_feature
     @cv_features = @game.calculate_cv_features(params, false)
+    calculate_feature_options
     respond_to do |format|
       format.js
     end
