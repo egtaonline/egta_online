7c9356d05490feab2701b5dc626ffa8175e8d017
Ben-Alexander Cassell <bcassell@umich.edu>
2011-04-26 17:44:15 -0400

    cv

--- app/models/control_variate.rb
+++ app/models/control_variate.rb
@@ -14,7 +14,7 @@ class ControlVariate
     adjustment_coefficient_record = Game.find(source_id).adjustment_coefficient_records.create!
     adjustment_coefficient_record.save!
     self.adjustment_coefficient_record_id = adjustment_coefficient_record.id
-    Stalker.enqueue 'calculate_cv', :game => self.game.id, :source_game => source_id, :cv => self.id, :name => "#{game.name}:cv:#{source_id}"
+    Stalker.enqueue 'calculate_cv', :game => self.game.id.to_s, :source_game => source_id.to_s, :cv => self.id.to_s, :name => "#{game.name}:cv:#{source_id}"
   end
 
   def transform_game(name)
