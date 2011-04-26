653b99498e42b10553ce334ce44bf88f264032f4
Ben-Alexander Cassell <bcassell@umich.edu>
2011-04-26 14:51:19 -0400

    cv

--- app/models/control_variate.rb
+++ app/models/control_variate.rb
@@ -16,32 +16,38 @@ class ControlVariate
 
   def apply_cv(source_id, features)
     self.acr_game_id = source_id
-    @adjustment_coefficient_record = Game.find(source_id).adjustment_coefficient_records.create!
-    @adjustment_coefficient_record.save!
+    adjustment_coefficient_record = Game.find(source_id).adjustment_coefficient_records.create!
+    adjustment_coefficient_record.save!
     self.adjustment_coefficient_record_id = @adjustment_coefficient_record.id
-    @adjustment_coefficient_record.calculate_coefficients(features.collect {|x| Game.find(source_id).features.where(:name => x).first})
-    g = transform_game(game, ":cv")
+    adjustment_coefficient_record.calculate_coefficients(self.features.collect {|x| Game.find(source_id).features.where(:name => x).first})
+    g = transform_game("#{game.name}:cv:#{source_id}")
     self.update_attributes(:destination_id => g.id)
   end
 
-  def apply_transformation(g)
-    g.features.each {|f| if f.expected_value == nil; f.update_attributes(:expected_value => f.sample_avg); end}
+  def transform_game(name)
+    trans_game = Game.new(:name => name)
+    self.game.simulator.games < trans_game
+    trans_game.save!
+    self.game.strategies.each {|strat| trans_game.strategies.create!(:name => strat.name)}
+    self.game.features.each {|f| if f.expected_value == nil; f.update_attributes(:expected_value => f.sample_avg); end}
     adjustment_coefficient_record = Game.find(self.acr_game_id).adjustment_coefficient_records.find(self.adjustment_coefficient_record_id)
-    g.profiles.each do |x|
-      x.players.each do |y|
-        y.payoffs.each do |z|
-          adjusted = z.payoff
-          g.features.each do |f|
+    self.game.profiles.each do |profile|
+      trans_profile = trans_game.profiles.create!
+      profile.players.each do |player|
+        trans_player = trans_profile.players.create!
+        player.payoffs.each do |payoff|
+          adjusted = payoff.payoff
+          self.features.each do |f|
             if adjustment_coefficient_record.feature_hash[f.name] != nil
-              adjusted += adjustment_coefficient_record.feature_hash[f.name].to_f*(f.feature_samples.where(:sample_id => z.sample_id).first.value - f.expected_value)
+              adjusted += adjustment_coefficient_record.feature_hash[f.name].to_f*(f.feature_samples.where(:sample_id => payoff.sample_id).first.value - f.expected_value)
             end
           end
-          z.update_attributes(:payoff => adjusted)
+          trans_player.payoffs.create!(:payoff = adjusted, :sample_id = payoff.sample_id)
         end
       end
     end
-    if g.save!
-      return g
+    if trans_game.save!
+      return trans_game
     else
       nil
     end
