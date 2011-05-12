9101a2d68356ceed9db05b884488943d9bd5abe6
Ben-Alexander Cassell <bcassell@umich.edu>
2011-05-11 23:49:07 -0400

    god

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,20 +13,32 @@ role :web, "d-108-249.eecs.umich.edu"                          # Your HTTP serve
 role :app, "d-108-249.eecs.umich.edu"                          # This may be the same as your `Web` server
 role :db,  "d-108-249.eecs.umich.edu", :primary => true # This is where Rails migrations will run
 
-namespace :deploy do
-  task :stop_god do
-    run "god terminate"
+namespace :god do
+  task :start, :roles => :app do
+    god_config_file = "#{latest_release}/config/egta.god"
+    sudo "god --log-level debug -c #{god_config_file}"
   end
-
-  task :start_god do
-    run "god -c #{current_release}/config/egta.god"
+  task :stop, :roles => :app do
+    sudo "god terminate" rescue nil
   end
-
-  desc "Restart Application"
   task :restart, :roles => :app do
-    before "deploy:symlink", "deploy:stop_god"
+    god.stop
+    god.start
+  end
+  task :status, :roles => :app do
+    sudo "god status"
+  end
+  task :log, :roles => :app do
+    sudo "tail -f /var/log/messages"
+  end
+  task :deploy_config, :roles => :app do
+    god_config_file = "#{latest_release}/config/omt.god"
+    god_script_template = File.dirname(__FILE__) + "/deploy/templates/omt.god.erb.rb"
+    data = ERB.new(IO.read(god_script_template)).result(binding)
+    sudo "god load #{god_config_file}"
+  end
+  task :redeploy, :roles => :app do
+    god.deploy_config
+    god.load_config
   end
 end
-
-before 'deploy:update_code', 'deploy:stop_god'
-after "deploy:symlink", "deploy:start_god"
