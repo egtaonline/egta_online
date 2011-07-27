f6065a413a28983f94b8c0f16a891e3516c506b0
Ben-Alexander Cassell <bcassell@umich.edu>
2011-07-27 14:10:27 -0400

    simulators

--- app/models/analyzable.rb
+++ app/models/analyzable.rb
@@ -1,5 +1,5 @@
 class Analyzable
   include Mongoid::Document
-  include Mongoid::Timestamps::Updated
+#  include Mongoid::Timestamps::Updated
   has_many :analysis_items, :inverse_of => :analyzable
 end
