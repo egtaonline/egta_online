b68ba9bf7fdd1273f5cdb4aaaf7ca62a3b62fde2
Ben-Alexander Cassell <bcassell@umich.edu>
2011-07-26 23:08:33 -0400

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,10 +6,10 @@ EgtaOnline::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
