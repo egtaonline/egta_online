30910ae2d235649bdcf2faf7eec7112ff6849d49
Ben-Alexander Cassell <bcassell@umich.edu>
2011-07-28 20:52:05 -0400

    adding std dev to profiles

--- app/models/profile_entry.rb
+++ app/models/profile_entry.rb
@@ -7,4 +7,10 @@ class ProfileEntry
   field :name
 
   validates_presence_of :name
+
+  def std_dev
+    mean = samples.avg(:payoff)
+    Math.sqrt(samples.reduce(0){|sum, u| sum + (u.payoff - mean) ** 2 } / (samples.length.to_f-1.0) )
+  end
+
 end
