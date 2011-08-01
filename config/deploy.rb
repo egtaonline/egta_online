760d8147d1ce0a292f4130327fe8d017b69de792
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-01 19:29:42 -0400

    async

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,11 +24,11 @@ namespace :deploy do
 
   task :stop_god do
     run "/usr/local/rvm/bin/bootup_god terminate" rescue nil
-    run "sudo /etc/init.d/redis-server stop" rescue nil
+    run "rvmsudo /etc/init.d/redis-server stop" rescue nil
   end
 
   task :start_god do
-    run "sudo /etc/init.d/redis-server start"
+    run "rvmsudo /etc/init.d/redis-server start"
     run "/usr/local/rvm/bin/bootup_god -c #{current_release}/config/egta.god"
   end
 
