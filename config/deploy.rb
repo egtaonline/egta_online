e626279c2e4d8ccf54549ceb84a01b96fac10d85
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-19 21:24:12 -0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 require 'bundler/capistrano'
-set :rvm_ruby_string, 'ruby-1.9.2-p290@railspre'
+set :rvm_ruby_string, 'ruby-1.9.2'
 
 set :application, "EGTMAS Web Interface"
 set :repository,  "git@github.com:egtaonline/egta_online.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :deploy_to, "/home/bcassell/Deployment"
-role :web, "d-108-249.eecs.umich.edu"                          # Your HTTP server, Apache/etc
-role :app, "d-108-249.eecs.umich.edu"                          # This may be the same as your `Web` server
-role :db,  "d-108-249.eecs.umich.edu", :primary => true # This is where Rails migrations will run
+set :deploy_to, "/home/deployment"
+role :web, "deployment@d-108-249.eecs.umich.edu"                          # Your HTTP server, Apache/etc
+role :app, "deployment@d-108-249.eecs.umich.edu"                          # This may be the same as your `Web` server
+role :db,  "deployment@d-108-249.eecs.umich.edu", :primary => true # This is where Rails migrations will run
 
 namespace :deploy do
   task :make_upload_dir do
@@ -43,7 +43,7 @@ namespace :deploy do
   end
 end
 
-before 'deploy:update_code', 'deploy:stop_god'
+#before 'deploy:update_code', 'deploy:stop_god'
 after 'deploy:update_code', 'deploy:make_upload_dir'
 before 'deploy:symlink', 'deploy:precompile_assets'
-after "deploy:symlink", "deploy:start_god"
+#after "deploy:symlink", "deploy:start_god"
