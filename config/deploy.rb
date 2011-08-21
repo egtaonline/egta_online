ea4003b5f3d166d28ecb9abb53f616239c558483
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-20 22:36:25 -0400

    resque god

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 require 'bundler/capistrano'
+default_run_options[:pty] = true
 set :rvm_ruby_string, 'ruby-1.9.2'
 set :rvm_type, :user
 set :application, "EGTMAS Web Interface"
