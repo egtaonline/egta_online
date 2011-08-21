46cd0974f2870dc0e3fabebb456b2310a866b1b4
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-20 21:48:22 -0400

    resque work

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,9 @@
 require 'resque_scheduler'
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+unless defined?(RESQUE_LOGGER)
+  RESQUE_LOGGER =
+ActiveSupport::BufferedLogger.new("#{Rails.root}/log/resque.log")
+  RESQUE_LOGGER.auto_flushing = true
+end
 Resque::NYX_PROXY = ServerProxy.new
 Resque::NYX_PROXY.start
