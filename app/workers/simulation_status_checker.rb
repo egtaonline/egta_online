e1b0bbc5e697b01dddc5315d9a95137b29c974be
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-22 15:18:24 -0400

    debugging workers

--- app/workers/simulation_status_checker.rb
+++ app/workers/simulation_status_checker.rb
@@ -5,13 +5,14 @@ class SimulationStatusChecker
 
     simulation = Simulation.find(simulation_id) rescue nil
     if simulation != nil
-      puts "checking status"
+      puts "checking against jobs"
       simulator = simulation.scheduler.simulator
       root_path = "#{Yetting.deploy_path}/#{simulator.fullname}/#{simulator.name}"
       if job_id.include?(simulation.job_id)
         state = state_info[job_id.index(simulation.job_id)][9]
         puts state_info
         if state == "C"
+          puts "checking existance"
           if check_existance(root_path, simulation)
             server = Resque::NYX_PROXY.sessions.servers_for(:scheduling).flatten.detect{|serv| serv.user == simulation.account.username}
             server.session(true).scp.download!("#{root_path}/../simulations/#{simulation.number}", "#{Rails.root}/db/", :recursive => true)
@@ -21,9 +22,12 @@ class SimulationStatusChecker
           simulation.start!
         end
       else
+        puts "checking existance"
         if check_existance(root_path, simulation)
           server = Resque::NYX_PROXY.sessions.servers_for(:scheduling).flatten.detect{|serv| serv.user == simulation.account.username}
+          puts "downloading"
           server.session(true).scp.download!("#{root_path}/../simulations/#{simulation.number}", "#{Rails.root}/db/", :recursive => true)
+          puts "checking for errors"
           check_for_errors(simulation)
         else
           simulation.failure!
@@ -40,8 +44,10 @@ class SimulationStatusChecker
   def self.check_for_errors(simulation)
     if File.open("#{Rails.root}/db/#{simulation.number}/out-#{simulation.number}").read == ""
       if File.exist?("#{Rails.root}/db/#{simulation.number}/payoff_data")
+        puts "enqueue data parsing"
         Resque.enqueue(DataParser, simulation.number)
       else
+        puts "missing payoff data"
         simulation.error_message = "Payoff data is missing, cause unknown."
         simulation.failure!
       end
