0c8dda14956ddc73ba9160f07a45b5fa58c27811
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-22 17:16:15 -0400

    server proxy module

--- lib/server_proxy.rb
+++ lib/server_proxy.rb
@@ -11,6 +11,9 @@ module ServerProxy
     @@staging_session = Net::SSH.start(Yetting.host, Account.first.username, :password => Account.first.password)
   end
   @@sessions.group :scheduling do
-    Account.all.each {|account| add_account(account)}
+    if @@staging_session == nil
+      @@staging_session = Net::SSH.start(Yetting.host, account.username, :password => account.password)
+    end
+    @@sessions.use(Yetting.host, :user => account.username, :password => account.password)
   end
 end
