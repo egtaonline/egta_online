39402db622e273840a5ca92ee7a9a72ff1a7a848
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-22 13:14:33 -0400

    cap and foreman

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-require 'bundler/capistrano'
-
 set :rvm_ruby_string, 'ruby-1.9.2'
 set :application, "egtaonline"
 set :repository,  "git@github.com:egtaonline/egta_online.git"
@@ -93,7 +91,7 @@ namespace :deploy do
   desc "precompile the assets"
   task :precompile_assets, :roles => :web, :except => { :no_release => true } do
     run "cd #{current_path}; rm -rf public/assets/*"
-    run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+    run "cd #{current_path}; RAILS_ENV=production sudo bundle exec rake assets:precompile"
   end
 
   desc "Disable requests to the app, show maintenance page"
