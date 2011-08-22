9290a56a65f215341e112b7f2b884f881b57eae5
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-22 17:14:37 -0400

    weird module

--- lib/server_proxy.rb
+++ lib/server_proxy.rb
@@ -1,17 +1,16 @@
 module ServerProxy
-  @@sessions = Net::SSH::Multi.start
-  if Account.all.count > 0
-    @@staging_session = Net::SSH.start(Yetting.host, Account.first.username, :password => Account.first.password)
-  end
-  @@sessions.group :scheduling do
-#    Account.all.each {|account| add_account(account)}
-  end
-
   def add_account(account)
     if @@staging_session == nil
       @@staging_session = Net::SSH.start(Yetting.host, account.username, :password => account.password)
     end
     @@sessions.use(Yetting.host, :user => account.username, :password => account.password)
   end
-
+  
+  @@sessions = Net::SSH::Multi.start
+  if Account.all.count > 0
+    @@staging_session = Net::SSH.start(Yetting.host, Account.first.username, :password => Account.first.password)
+  end
+  @@sessions.group :scheduling do
+    Account.all.each {|account| add_account(account)}
+  end
 end
