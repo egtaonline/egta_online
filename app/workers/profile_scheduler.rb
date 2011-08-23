1260ae147016c86703465ce2e3fa18bf5b70cc33
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-23 14:01:20 -0400

    scheduling

--- app/workers/profile_scheduler.rb
+++ app/workers/profile_scheduler.rb
@@ -8,6 +8,7 @@ class ProfileScheduler
       if profile.simulations.scheduled.count == 0
         sample_count = profile.simulations.active.scheduled.reduce(0) {|sum, sch| sum+sch.size} + profile.sample_count
         max_schedulable = profile.schedulers.active.collect {|s| s.max_samples}.push(0).max
+        puts max_schedulable
         if max_schedulable > sample_count
           scheduler = Scheduler.find(scheduler_id) rescue nil
           if scheduler != nil
