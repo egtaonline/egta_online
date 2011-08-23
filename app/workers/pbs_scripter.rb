d342ad62fef752ea3366579af0ead23d9958c99e
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-23 16:27:20 -0400

    scheduling

--- app/workers/pbs_scripter.rb
+++ app/workers/pbs_scripter.rb
@@ -49,7 +49,7 @@ class PBSScripter
       file.syswrite("\#PBS -e #{root_path}/../simulations/#{simulation.number}/out\n")
       file.syswrite("mkdir /tmp/${PBS_JOBID}; cd /tmp/${PBS_JOBID}; cp -r #{root_path}/* .; cp -r #{root_path}/../simulations/#{simulation.number} .\n")
       file.syswrite("/tmp/${PBS_JOBID}/#{simulation.number}/script/batch /tmp/${PBS_JOBID}/#{simulation.number} #{simulation.size}\n")
-      file.syswrite("cp -r #{simulation.number} #{root_path}/../simulations; /bin/rm -rf /tmp/${PBS_JOBID}")
+      file.syswrite("cp -r /tmp/${PBS_JOBID}/#{simulation.number} #{root_path}/../simulations; /bin/rm -rf /tmp/${PBS_JOBID}")
     end
   end
 
