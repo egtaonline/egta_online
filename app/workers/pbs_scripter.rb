81a2e2cdfb2e7ee0ccd934807b203ffbabfff500
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-23 16:03:16 -0400

    scheduling

--- app/workers/pbs_scripter.rb
+++ app/workers/pbs_scripter.rb
@@ -45,12 +45,10 @@ class PBSScripter
         file.syswrite("\n\#PBS -A cac\n\#PBS -q cac")
       end
       file.syswrite("\n\#PBS -N mas-#{simulator.name.downcase.gsub(' ', '_')}\n")
-      str = "\#PBS -t #{simulation.number}\n"
-      file.syswrite(str)
       file.syswrite("\#PBS -o #{root_path}/../simulations/#{simulation.number}/out\n")
       file.syswrite("\#PBS -e #{root_path}/../simulations/#{simulation.number}/out\n")
       file.syswrite("mkdir /tmp/${PBS_JOBID}; cd /tmp/${PBS_JOBID}; cp -r #{root_path}/* .; cp -r #{root_path}/../simulations/#{simulation.number} .\n")
-      file.syswrite("/tmp/#{simulation.number}/script/batch /tmp/${PBS_JOBID}/#{simulation.number} #{simulation.size}\n")
+      file.syswrite("/tmp/${PBS_JOBID}/#{simulation.number}/script/batch /tmp/${PBS_JOBID}/#{simulation.number} #{simulation.size}\n")
       file.syswrite("cp -r #{simulation.number} #{root_path}/../simulations; /bin/rm -rf /tmp/${PBS_JOBID}")
     end
   end
