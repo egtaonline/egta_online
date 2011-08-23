c2d8e5f48007cc94c5475e52761a641f3f5b6a93
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-23 14:04:42 -0400

    scheduling

--- app/workers/profile_scheduler.rb
+++ app/workers/profile_scheduler.rb
@@ -7,7 +7,9 @@ class ProfileScheduler
       puts "deciding to schedule a simulation for #{profile.proto_string}"
       if profile.simulations.scheduled.count == 0
         sample_count = profile.simulations.active.scheduled.reduce(0) {|sum, sch| sum+sch.size} + profile.sample_count
-        max_schedulable = profile.schedulers.active.collect {|s| s.max_samples}.push(0).max
+        max_array = profile.schedulers.active.collect {|s| s.max_samples}.push(0)
+        puts max_array
+        max_schedulable = max_array.max
         puts max_schedulable
         if max_schedulable > sample_count
           scheduler = Scheduler.find(scheduler_id) rescue nil
