c077fbd5956282f89328e333c1d1b355b9b3e22f
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-26 15:44:47 -0400

    adding hoptoad notification

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -12,3 +12,13 @@ unless defined?(RESQUE_LOGGER)
   RESQUE_LOGGER = ActiveSupport::BufferedLogger.new("#{Rails.root}/log/resque.log")
   RESQUE_LOGGER.auto_flushing = true
 end
+
+require 'resque/failure/multiple'
+require 'resque/failure/hoptoad'
+require 'resque/failure/redis'
+Resque::Failure::Hoptoad.configure do |config|
+  config.api_key = '860cc72b7fb01397170a67a7997a7322'
+  config.secure = true
+end
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Hoptoad]
+Resque::Failure.backend = Resque::Failure::Multiple
