dfcbb72db6ca0c9d08dea72929a002f5470605be
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-26 14:50:20 -0400

    security

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,6 @@ class Account
   field :username
   validates_presence_of :username
   validate :login
-  field :encrypted_password
   field :active, :type => Boolean, :default => false
   scope :active, where(active: true)
 
@@ -16,25 +15,9 @@ class Account
 
   def login
     begin
-      Net::SSH.start(Yetting.host, username, :password => self.password, :timeout => 2)
+      Net::SSH.start(Yetting.host, username, :timeout => 2)
     rescue
       errors.add(:username, "can't login to host")
-    end
-  end
-
-  def password=(pass)
-    if pass == "" || pass == nil
-      errors.add(:password, "can't be empty")
-    else
-      self.update_attributes(:encrypted_password => pass.encrypt(:symmetric, :password => "srgegta"))
-    end
-  end
-
-  def password
-    if self.encrypted_password == nil
-      ''
-    else
-      self.encrypted_password.decrypt(:symmetric, :password => "srgegta")
     end
   end
 end
