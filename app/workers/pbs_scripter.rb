3fe6b13a0af374a41680fa9cae80bb792912e815
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-25 20:39:15 -0400

    minor

--- app/workers/pbs_scripter.rb
+++ app/workers/pbs_scripter.rb
@@ -54,7 +54,7 @@ class PBSScripter
   def self.get_job(account, simulator, submission)
     job_return = ""
     if submission != nil
-      @sp.sessions.with(account.username.to_sym).exec("cd #{Yetting.deploy_path}/#{simulator.fullname}/#{simulator.name}/script; #{submission.command}") do |ch, stream, data|
+      channel = @sp.sessions.with(account.username.to_sym).exec("cd #{Yetting.deploy_path}/#{simulator.fullname}/#{simulator.name}/script; #{submission.command}") do |ch, stream, data|
         job_return = data
         puts "[#{ch[:host]} : #{stream}] #{data}"
         job_return.strip! if job_return != nil
