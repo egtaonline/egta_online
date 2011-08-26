1fa7ee070d79962afb1fe47a5f7c7046e38a869e
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-25 20:27:47 -0400

    minor

--- app/workers/folder_creator.rb
+++ app/workers/folder_creator.rb
@@ -13,7 +13,7 @@ class FolderCreator
       @sp.sftp.upload!("tmp/#{simulation.number}", "#{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}", owner: @sp.staging_session.options[:user], gid: WELLMAN)
       @sp.staging_session.exec!("chmod -R ug+rwx #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
       puts "hierarchy completed for #{simulation.number}"
-      FileUtils.rm_rf("tmp/400")
+      FileUtils.rm_rf("tmp/#{simulation.number}")
     end
   end
 end
