4a3fc5d39bd247e2c0352fb2e97dd7cb261a6044
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-25 20:17:46 -0400

    minor

--- app/workers/folder_creator.rb
+++ app/workers/folder_creator.rb
@@ -10,7 +10,7 @@ class FolderCreator
       Dir.mkdir("tmp/#{simulation.number}")
       Dir.mkdir("tmp/#{simulation.number}/features")
       FileUtils.mv("tmp/temp.yaml", "tmp/#{simulation.number}/simulation_spec.yaml")
-      @sp.sftp.upload!("tmp/#{simulation.number}", "#{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}", owner: @sp.staging_session.user, gid: WELLMAN)
+      @sp.sftp.upload!("tmp/#{simulation.number}", "#{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}", owner: @sp.staging_session[:user], gid: WELLMAN)
       @sp.staging_session.exec!("chmod -R ug+rwx #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
       puts "hierarchy completed for #{simulation.number}"
       FileUtils.rm_rf("tmp/400")
