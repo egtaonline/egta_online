559420682d416e434defa80af2049a0e22722623
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-26 15:27:06 -0400

    Cleaning up dirty exits

--- app/workers/dirty_exit_cleaner.rb
+++ app/workers/dirty_exit_cleaner.rb
@@ -3,7 +3,7 @@ class DirtyExitCleaner
 
   def self.perform
     puts "making sure pending simulations got queued"
-    Simulation.pending.all {|s| Resque.enqueue(SimulationQueuer, s.id)}
+    Simulation.pending.each {|s| Resque.enqueue(SimulationQueuer, s.id)}
     puts "ensuring profiles that are due for more simulations get them"
     Simulation.finished.where(updated_at:updated_at.gt => (Time.current-3600)).each {|s| s.requeue}
   end
