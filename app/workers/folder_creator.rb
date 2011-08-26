d74184efc76492a680e5470a9efdebb5b527fce7
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-25 22:58:35 -0400

    minor

--- app/workers/folder_creator.rb
+++ app/workers/folder_creator.rb
@@ -12,8 +12,6 @@ class FolderCreator
       Dir.mkdir("tmp/#{simulation.number}/features")
       FileUtils.mv("tmp/temp.yaml", "tmp/#{simulation.number}/simulation_spec.yaml")
       @sp.sftp.upload!("tmp/#{simulation.number}", "#{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}", owner: @sp.staging_session.options[:user], gid: WELLMAN)
-      @sp.staging_session.exec!("chmod -R ug+rwx #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
-      puts @sp.staging_session.exec!("ls -l #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
       puts "hierarchy completed for #{simulation.number}"
       FileUtils.rm_rf("tmp/#{simulation.number}")
     end
--- app/workers/pbs_scripter.rb
+++ app/workers/pbs_scripter.rb
@@ -16,6 +16,8 @@ class PBSScripter
       puts "creating wrapper"
       create_wrapper(simulation)
       puts "scheduling simulation"
+      @sp.staging_session.exec!("chmod -R ug+rwx #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
+      puts @sp.staging_session.exec!("ls -l #{Yetting.deploy_path}/#{simulator.fullname}/simulations/#{simulation.number}")
       @sp.sftp.upload!("#{Rails.root}/tmp/wrapper", "#{root_path}/script/wrapper", owner: @sp.staging_session.options[:user], gid: WELLMAN)
       @sp.staging_session.exec!("chmod -R ug+rwx #{root_path}")
       @job = get_job(simulation.account, simulator, submission)
