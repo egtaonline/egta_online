8f9045d5e72fe04ad2e48991abeb1732eab3d776
Ben-Alexander Cassell <bcassell@umich.edu>
2011-08-30 14:03:47 -0400

    fixing NO FILE errors

--- app/workers/simulation_queuer.rb
+++ app/workers/simulation_queuer.rb
@@ -39,49 +39,51 @@ class SimulationQueuer
 
   def self.schedule(simulations)
     Account.all.each do |account|
-      scp = Net::SCP.start(Yetting.host, account.username)
-      scp.upload!("tmp/#{account.username}", "#{Yetting.deploy_path}/simulations", recursive: true) do |ch, name, sent, total|
-        puts "#{name}: #{sent}/#{total}"
-      end
-      Net::SSH.start(Yetting.host, account.username) do |ssh|
-        simulations.where(account_id: account.id).each do |s|
-          begin
-            simulator = s.scheduler.simulator
-            root_path = "#{Yetting.deploy_path}/#{simulator.fullname}/#{simulator.name}"
-            puts "creating submission"
-            submission = Submission.new(s.scheduler, s.size, s.number, "#{Yetting.deploy_path}/simulations/#{account.username}/#{s.number}/wrapper")
-            if (Simulation.active.flux.count+1) <= FLUX_LIMIT
-              s.update_attribute(:flux, true)
-              submission.qos = "wellman_flux"
-            end
-            puts "scheduling simulation"
-            if submission != nil
-              channel = ssh.exec("#{submission.command}") do |ch, stream, data|
-                if stream == :std_err
-                  puts "submission failed"
-                  s.error_message = "submission failed: #{data}"
-                  s.failure!
-                else
-                job_return = data
-                  puts "[#{ch[:host]} : #{stream}] #{data}"
-                  job_return.strip! if job_return != nil
-                  job_return = job_return.split(".").first
-                  if job_return != "" and job_return != nil and is_a_number?(job_return)
-                    s.send('queue!')
-                    s.job_id = job_return
-                    s.save!
-                  else
+      if simulations.where(account_id: account.id).count > 0
+        scp = Net::SCP.start(Yetting.host, account.username)
+        scp.upload!("tmp/#{account.username}", "#{Yetting.deploy_path}/simulations", recursive: true) do |ch, name, sent, total|
+          puts "#{name}: #{sent}/#{total}"
+        end
+        Net::SSH.start(Yetting.host, account.username) do |ssh|
+          simulations.where(account_id: account.id).each do |s|
+            begin
+              simulator = s.scheduler.simulator
+              root_path = "#{Yetting.deploy_path}/#{simulator.fullname}/#{simulator.name}"
+              puts "creating submission"
+              submission = Submission.new(s.scheduler, s.size, s.number, "#{Yetting.deploy_path}/simulations/#{account.username}/#{s.number}/wrapper")
+              if (Simulation.active.flux.count+1) <= FLUX_LIMIT
+                s.update_attribute(:flux, true)
+                submission.qos = "wellman_flux"
+              end
+              puts "scheduling simulation"
+              if submission != nil
+                channel = ssh.exec("#{submission.command}") do |ch, stream, data|
+                  if stream == :std_err
                     puts "submission failed"
-                    s.error_message = "submission failed: #{job_return}"
+                    s.error_message = "submission failed: #{data}"
                     s.failure!
+                  else
+                  job_return = data
+                    puts "[#{ch[:host]} : #{stream}] #{data}"
+                    job_return.strip! if job_return != nil
+                    job_return = job_return.split(".").first
+                    if job_return != "" and job_return != nil and is_a_number?(job_return)
+                      s.send('queue!')
+                      s.job_id = job_return
+                      s.save!
+                    else
+                      puts "submission failed"
+                      s.error_message = "submission failed: #{job_return}"
+                      s.failure!
+                    end
                   end
                 end
+                channel.wait
               end
-              channel.wait
+            rescue
+              s.error_message = "failed in the submission step"
+              s.failure!
             end
-          rescue
-            s.error_message = "failed in the submission step"
-            s.failure!
           end
         end
       end
