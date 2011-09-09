fbeaa50db06ddd7e011e4aeca47d4d2f092b2048
Ben-Alexander Cassell <bcassell@umich.edu>
2011-09-09 16:29:21 -0400

    fixing strategy delete

--- app/workers/data_parser.rb
+++ app/workers/data_parser.rb
@@ -4,32 +4,20 @@ class DataParser
   @queue = :nyx_actions
 
   def self.perform(number, location="#{Rails.root}/db/#{Simulation.where(number: number).first.account.username}")
+    puts "loading feature values" 
     feature_hash = create_feature_hash(number, location)
-    (Dir.entries(location+"/#{number}/features")-[".", ".."]).each {|x| feature_files[x] = File.open(location+"/#{number}/features/"+x) }
-    keys = feature_files.keys
-    unless keys.size == 0
-      puts "loading feature types"
-      YAML.load_documents(feature_files[keys[0]]) do |doc|
-        feature_hash_array << Hash.new
-        feature_hash_array.last[keys[0]] = doc
-      end
-    end
-    puts "loading feature values"
-    keys.each_index do |index|
-      unless index == 0
-        count = 0
-        YAML.load_documents(feature_files[keys[index]]) do |doc|
-          feature_hash_array[count][keys[index]] = doc
-          count += 1
-        end
-      end
-    end
     payoff_data = Array.new
     puts "opening payoff_data"
     File.open(location+"/#{number}/payoff_data") {|io| YAML.load_documents(io) {|yf| payoff_data << yf }}
     begin
       puts "storing payoff data"
-      store_in_profile(Simulation.where(:number => number).first.profile, payoff_data, feature_hash_array)
+      payoff_data.size.times do |i|
+        feature_hash_record = {}
+        feature_hash.keys.each do |key|
+          feature_hash_record[key] = feature_hash[key][i]
+        end
+        Simulation.where(:number => number).first.profile.sample_records.create!(payoffs: payoff_data[i], feature_hash_record)
+      end
       Simulation.where(number: number).first.finish!
     rescue
       puts "malformed payoff data"
@@ -41,8 +29,11 @@ class DataParser
   def self.create_feature_hash(number, location)
     feature_hash = {}
     (Dir.entries(location+"/#{number}/features")-[".", ".."]).each do |x|
-      File.open(location+"/#{number}/features") do |f|
-        
+      File.open(location+"/#{number}/features/#{x}") do |f|
+        YAML.load_documents(f) do |doc|
+          feature_hash[x] = [] if feature_hash[x] == nil
+          feature_hash[x] << doc
+        end
       end
     end
   end
