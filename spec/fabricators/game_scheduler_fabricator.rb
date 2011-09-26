a94abfbdd68ad686797596e79617fb43a8f882a9
Ben-Alexander Cassell <bcassell@umich.edu>
2011-09-26 13:24:46 -0400

    gem

---
+++ spec/fabricators/game_scheduler_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:symmetric_game_scheduler) do
+  name { Fabricate.sequence(:name) { |i| "testing#{i}" } }
+  size 2
+  process_memory 1000
+  samples_per_simulation 1
+  max_samples 2
+  time_per_sample 40
+  after_create {|sgs| if sgs.parameter_hash.is_a?(String); sgs.update_attribute(:parameter_hash, eval(sgs.parameter_hash)); end }
+end
