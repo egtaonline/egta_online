4a02d00351be63c0c0be9d44427a3ce5a0c2cc84
Ben-Alexander Cassell <bcassell@umich.edu>
2011-09-26 11:54:08 -0400

    patches

--- app/models/sample_record.rb
+++ app/models/sample_record.rb
@@ -3,6 +3,5 @@ class SampleRecord
   belongs_to :profile
   field :payoffs, type: Hash
   field :features, type: Hash
-  validates_uniqueness_of :payoffs
   after_create {profile.update_avgs_and_stds(self)}
 end
