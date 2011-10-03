3d5033686e7115e67a9802bac3c4971fa979fc8f
Ben-Alexander Cassell <bcassell@umich.edu>
2011-10-03 14:50:50 -0400

    simulator upload fix

--- app/workers/simulator_initializer.rb
+++ app/workers/simulator_initializer.rb
@@ -2,14 +2,15 @@ class SimulatorInitializer
   @queue = :nyx_actions
 
   def self.perform(simulator_id)
-    @sp ||= ServerProxy.instance
     simulator = Simulator.find(simulator_id) rescue nil
     if simulator != nil
       puts "setting up simulator"
-      @sp.staging_session.exec!("rm -rf #{Yetting.deploy_path}/#{simulator.fullname}*; rm -rf #{Yetting.deploy_path}/#{simulator.name}.zip")
-      @sp.staging_session.scp.upload!(simulator.simulator_source.path, Yetting.deploy_path)
-      @sp.staging_session.exec!("cd #{Yetting.deploy_path}; unzip -uqq #{simulator.name}.zip -d #{simulator.fullname}; mkdir #{simulator.fullname}/simulations")
-      @sp.staging_session.exec!("cd #{Yetting.deploy_path}; chmod -R ug+rwx #{simulator.fullname}")
+      account = Account.all.sample
+      ssh = Net::SSH.start(Yetting.host, account.username)
+      ssh.exec!("rm -rf #{Yetting.deploy_path}/#{simulator.fullname}*; rm -rf #{Yetting.deploy_path}/#{simulator.name}.zip")
+      ssh.scp.upload!(simulator.simulator_source.path, Yetting.deploy_path)
+      ssh.exec!("cd #{Yetting.deploy_path}; unzip -uqq #{simulator.name}.zip -d #{simulator.fullname}; mkdir #{simulator.fullname}/simulations")
+      ssh.exec!("cd #{Yetting.deploy_path}; chmod -R ug+rwx #{simulator.fullname}")
     end
   end
 end
