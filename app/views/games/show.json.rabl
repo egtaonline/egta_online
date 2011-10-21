a049f7a4ebfd2b0e1c55c5e826d237e3a214cb06
Ben-Alexander Cassell <bcassell@umich.edu>
2011-10-21 00:50:37 -0400

    json fix

--- app/views/games/show.json.rabl
+++ app/views/games/show.json.rabl
@@ -3,6 +3,7 @@ child :roles do
   attributes :name, :count, :strategy_array
 end
 child @profiles => :profiles do
+  attributes :proto_string
   child :role_instances => :roles do
     attributes :name
     child :strategy_instances => :strategies do
--- app/workers/profile_gatherer.rb
+++ app/workers/profile_gatherer.rb
@@ -4,9 +4,6 @@ class ProfileGatherer
   def self.perform(game_id)
     game = Game.find(game_id) rescue nil
     if game != nil
-      puts "adding profiles to #{game.name}"
-      Profile.where(simulator_id: game.simulator_id, parameter_hash: game.parameter_hash, size: game.size).map(&:_id)
-      puts game.profile_ids
       game.update_attribute(:profile_ids, Profile.where(simulator_id: game.simulator_id, parameter_hash: game.parameter_hash, size: game.size).map(&:_id))
     end
   end
