eef1be06ea47b4e4134ca9ec62511cc1372adcbf
bcassell <bcassell@umich.edu>
2012-01-06 20:17:47 -0500

    strategy indexing

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -3,6 +3,8 @@ class Strategy
   include Mongoid::Sequence
   field :name
   field :number, :type=>Integer
+  index :name
+  index :number
   sequence :number
   validates_presence_of :name
   validates_uniqueness_of :name
