e451976e262d54bb63af3d7ec0afaef2946d607e
Ben-Alexander Cassell <bcassell@umich.edu>
2012-02-06 14:35:59 -0500

    refactoring

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,3 @@
-class AccountsController < EntitiesController
+class AccountsController < ApplicationController
+  
 end
--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -2,18 +2,16 @@ class EntitiesController < ApplicationController
   respond_to :html
   expose(:resource_class){controller_name.classify}
   expose(:resource) do
-    proxy = resource_class.constantize
+    klass = resource_class.constantize
     name = controller_name.singularize
-    if id = params["#{name}_id"] || params[:id]
-      proxy.find(id).tap do |r|
+    if id = params[:id]
+      klass.find(id).tap do |r|
         if request.get? == false
-          params[name] = params[name].merge(params[:selector]) if params[:selector] != nil
           r.attributes = params[name]
         end
       end
     else
-      params[name] = params[name].merge(params[:selector]) if params[:selector] != nil
-      proxy.new(params[name])
+      klass.new(params[name])
     end
   end
   expose(:resources){resource_class.constantize.page(params[:page])}
