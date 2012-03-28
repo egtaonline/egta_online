a87cbddb8fa12e9b21f0d048f11c168ee4dbc3b2
Ben-Alexander Cassell <bcassell@umich.edu>
2012-03-28 16:46:44 -0400

    migration

--- db/migrate/20120328184924_simplifying_strategies.rb
+++ db/migrate/20120328184924_simplifying_strategies.rb
@@ -7,10 +7,12 @@ end
 class SimplifyingStrategies < Mongoid::Migration
   
   def self.up
+    puts Profile.count
     db = Simulator.db
     Simulator.all.each do |s|
       s.roles.each do |r|
-        Strategy.find(r.strategy_ids).each do |strat|
+        r.strategies = []
+        Strategy.where(:_id.in => r.strategy_ids).each do |strat|
           r.strategies << strat.name
         end
       end
@@ -18,6 +20,36 @@ class SimplifyingStrategies < Mongoid::Migration
       s.reload
       puts s.roles.inspect
     end
+    puts Profile.count
+    Game.all.each do |g|
+      g.roles.each do |r|
+        r.strategies = []
+        Strategy.where(:_id.in => r.strategy_ids).each do |strat|
+          r.strategies << strat.name
+        end
+      end
+      g.save
+      g.reload
+      puts g.roles.inspect
+    end
+    puts Profile.count
+    Scheduler.all.each do |s|
+      if s.is_a?(GenericScheduler) == false
+        s.roles.each do |r|
+          r.strategies = []
+          Strategy.where(:_id.in => r.strategy_ids).each do |strat|
+            r.strategies << strat.name
+          end
+        end
+        s.save
+        s.reload
+        puts s.roles.inspect
+      end
+    end
+    puts Profile.count
+    db.collection("strategies").drop
+    puts Profile.count
+    db.collection("profiles").update({}, {"$unset" => {"proto_string" => 1}}, multi: true)
   end
 
   def self.down
