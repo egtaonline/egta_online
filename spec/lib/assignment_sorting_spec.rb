f7333d2e5fb2c9e538e238f252c9b34f6d238e6f
Ben-Alexander Cassell <bcassell@umich.edu>
2012-07-17 17:39:45 -0400

    removing unneeded artifacts

--- spec/lib/assignment_sorting_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe "String#assignment_sort" do
-  let(:unsorted_string){ "B: 1 D, 2 E; A: 3 G, 2 F" }
-  it { unsorted_string.assignment_sort.should == "A: 2 F, 3 G; B: 1 D, 2 E" }
-end
