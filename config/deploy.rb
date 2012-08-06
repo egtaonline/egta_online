e8af252c62a0baf8a41cb7145cb80df7ed3541fb
Ben-Alexander Cassell <bcassell@umich.edu>
2012-08-06 18:29:08 -0400

    tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,68 +15,110 @@ set :deploy_to, "/home/deployment"
 set :scm, :git
 set :repository,  "git@github.com:egtaonline/egta_online.git"
 set :branch, "origin/master"
+set :migrate_target,  :current
+set :ssh_options,     { :forward_agent => true }
+set :rails_env,       "production"
+set :normalize_asset_timestamps, false
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+default_environment["RAILS_ENV"] = 'production'
 
 default_run_options[:pty] = true
+
 namespace :deploy do
-  desc "Deploy app"
+  desc "Deploy your application"
   task :default do
     update
     restart
-    cleanup
   end
 
-  desc "Setup a GitHub-style deployment."
+  desc "Setup your git-based deployment app"
   task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
     run "git clone #{repository} #{current_path}"
+  end
+
+  task :cold do
+    update
+  end
+
+  task :update do
+    transaction do
+      update_code
+    end
   end
 
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
-    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}; bundle --without development test"
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
+  end
+
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids #{latest_release}/simulator_uploads &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -s #{shared_path}/simulator_uploads #{latest_release}/simulator_uploads &&
+      ln -sf #{shared_path}/mongoid.yml #{latest_release}/config/mongoid.yml
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
   end
 
   namespace :rollback do
-    desc "Rollback"
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
     task :default do
-      code
-    end
-
-    desc "Rollback a single commit."
-    task :code, :except => { :no_release => true } do
-      set :branch, "HEAD^"
-      default
+      rollback.repo
+      rollback.cleanup
     end
   end
+end
 
-  desc "Make all the symlinks"
-  task :symlink, :roles => :app, :except => { :no_release => true } do
-    set :normal_symlinks, %w(
-      simulator_uploads
-    )
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
+end
 
-    commands = normal_symlinks.map do |path|
-      "rm -rf #{current_path}/#{path} && \
-       ln -s #{shared_path}/#{path} #{current_path}/#{path}"
-    end
-
-    # set :weird_symlinks, {
-    #   "path_on_disk" => "path_to_symlink"
-    # }
-    commands += ["rm -rf #{current_path}/public/system && rm -rf #{current_path}/config/mongoid.yml && \
-     ln -s #{shared_path}/system #{current_path}/public/system && ln -s #{shared_path}/mongoid.yml #{current_path}/config/mongoid.yml"]
-    # end
-
-    # needed for some of the symlinks
-    run "mkdir -p #{current_path}/tmp && \
-         mkdir -p #{current_path}/log && \
-         mkdir -p #{current_path}/simulator_uploads"
-
-    run <<-CMD
-      cd #{current_path} &&
-      #{commands.join(" && ")}
-    CMD
-  end
-
+namespace :deploy do
   desc "Disable requests to the app, show maintenance page"
   web.task :disable, :roles => :web do
     run "cp #{current_path}/public/maintenance.html  #{shared_path}/system/maintenance.html"
@@ -122,8 +164,8 @@ namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      if capture("cd #{current_path} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{current_path} && #{rake} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
       end
@@ -131,6 +173,4 @@ namespace :deploy do
   end
 end
 
-before 'deploy:symlink', 'deploy:assets:precompile'
-after 'deploy:assets:precompile', 'foreman:restart'
-after "deploy", "deploy:cleanup"
+after 'deploy:symlink', 'foreman:restart'
