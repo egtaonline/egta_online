c4794a143273e9ae8e5ad0aebb4717e2fc28820b
Ben-Alexander Cassell <bcassell@umich.edu>
2012-08-08 11:59:52 -0400

    minor

--- config/initializers/backend.rb
+++ config/initializers/backend.rb
@@ -4,7 +4,7 @@ Backend.configure do |config|
   config.queue_periodicity = 5.minutes
   config.queue_quantity = 30
   config.backend_implementation.flux_active_limit = 120
-  if Rails.env.production? && !(File.basename( $0 ) == "rake" && (ARGV == ["db:migrate"] || ARGV.last == "assets:precompile"))
+  if (Rails.env.production? && !(File.basename( $0 ) == "rake" && (ARGV == ["db:migrate"] || ARGV.last == "assets:precompile")))
     config.backend_implementation.setup_connections
   end
 end
