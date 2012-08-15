07eb3153cf9c58fec9de6171fa3a098d00f23b52
Ben-Alexander Cassell <bcassell@umich.edu>
2012-08-15 14:39:13 -0400

    unicorns

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -11,7 +11,7 @@
 set -e
 
 # Feel free to change any of the following variables for your app:
-TIMEOUT=${TIMEOUT-60}
+TIMEOUT=${TIMEOUT-600}
 APP_ROOT=/home/deployment/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
