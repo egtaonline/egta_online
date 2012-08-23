2cebbdd44c6f8a9bcfe6090fec2cc96fbcaa9749
bcassell <bcassell@umich.edu>
2012-08-23 14:17:36 -0400

    debugging

--- lib/backend/flux/simulation_status_service.rb
+++ lib/backend/flux/simulation_status_service.rb
@@ -9,6 +9,7 @@ class SimulationStatusService
     if output != "" && output != nil
       output.split("\n").each{|line| parsed_output[line.split(".").first] = line.split(/\s+/)[9]}
     end
+    puts parsed_output
     parsed_output
   end
 end
