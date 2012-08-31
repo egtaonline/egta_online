d1528a2ef4562636ad8c26a48f72932830f01643
bcassell <bcassell@umich.edu>
2012-08-31 13:38:02 -0400

    fixed migration

--- db/migrate/20120822153148_reorganizing_observations.rb
+++ db/migrate/20120822153148_reorganizing_observations.rb
@@ -1,7 +1,7 @@
 class ReorganizingObservations < Mongoid::Migration
   def self.up
-    pool = Migrater.pool(size: 4)
-    Profile.where(:sample_count.gt => 0, :observations.exists => false).batch_size(10).each do |profile|
+    pool = Migrater.pool(size: 8)
+    Mongoid.default_session[:profiles].find(sample_count: { '$gt' => 0 }, observations: { '$exists' => false }).each do |profile|
       pool.migrate!(profile)
     end
   end
@@ -13,21 +13,37 @@ class ReorganizingObservations < Mongoid::Migration
     include Celluloid
 
     def migrate(profile)
+      if !profile['symmetry_groups']
+        puts "broken #{profile['_id']}"
+        return
+      end
       t = Time.now
-      o_array = Array.new(profile.sample_count){ { features: nil, symmetry_groups: [] } }
+      o_array = Array.new(profile['sample_count']){ { features: nil, symmetry_groups: [] } }
       profile['features_observations'].each { |f| o_array[f['observation_id']-1][:features] = f['features'] } if profile['features_observations']
       o_array = get_symmetry_groups(profile, o_array)
-      o_array.each do |obv|
-        profile.observations.create!(obv)
+      symmetry_groups = profile['symmetry_groups'].collect do |symmetry_group|
+        pcount = symmetry_group['players'].size
+        symmetry_group[:payoff] = symmetry_group['players'].collect{ |player| player['payoff'] }.reduce(:+).to_f/pcount
+        payoffs_squared = symmetry_group['players'].collect{ |player| player['payoff']**2.0 }.reduce(:+).to_f/pcount
+        squared_payoffs = symmetry_group[:payoff]**2.0
+        if squared_payoffs < payoffs_squared
+          symmetry_group[:payoff_sd] = 0.0
+        else
+          symmetry_group[:payoff_sd] = Math.sqrt(squared_payoffs-payoffs_squared)
+        end
+        symmetry_group
       end
-      p "finish #{Time.now-t}"
+      profile[:observations] = o_array
+      profile[:symmetry_groups] = symmetry_groups
+      Mongoid.default_session[:profiles].find(_id: profile['_id']).update(profile)
+      p "finish #{Time.now-t} #{profile['_id']}"
     end
 
     def get_symmetry_groups(profile, o_array)
-      profile.symmetry_groups.each do |symmetry_group|
-        o_array.each { |entry| entry[:symmetry_groups] << { role: symmetry_group.role, strategy: symmetry_group.strategy, count: symmetry_group.count, players: [], payoff: 0.0, payoff_sd: 0.0 } }
-        symmetry_group.players.each do |player|
-          o_array[player['observation_id']-1][:symmetry_groups].last[:players] << { payoff: player.payoff, features: player.features }
+      profile['symmetry_groups'].each do |symmetry_group|
+        o_array.each { |entry| entry[:symmetry_groups] << { role: symmetry_group['role'], strategy: symmetry_group['strategy'], count: symmetry_group['count'], players: [], payoff: 0.0, payoff_sd: 0.0 } }
+        symmetry_group['players'].each do |player|
+          o_array[player['observation_id']-1][:symmetry_groups].last[:players] << { payoff: player['payoff'], features: player['features'] }
         end
         o_array.each do |entry|
           sgroup = entry[:symmetry_groups].last
@@ -41,15 +57,6 @@ class ReorganizingObservations < Mongoid::Migration
             sgroup[:payoff_sd] = Math.sqrt(squared_payoffs-payoffs_squared)
           end
         end
-        symmetry_group.payoff = symmetry_group.players.avg(:payoff)
-        payoffs_squared = symmetry_group.players.collect{ |player| player.payoff**2.0 }.reduce(:+).to_f/symmetry_group.players.count
-        squared_payoffs = symmetry_group.payoff**2.0
-        if squared_payoffs < payoffs_squared
-          symmetry_group.payoff_sd = 0.0
-        else
-          symmetry_group.payoff_sd = Math.sqrt(squared_payoffs-payoffs_squared)
-        end
-        symmetry_group.save!
       end
       o_array
     end
