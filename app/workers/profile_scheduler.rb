8eea9057bdadae8ff7e41581ce2cf855732bedf1
bcassell <bcassell@umich.edu>
2012-08-31 15:20:55 -0400

    minor fix

--- app/workers/profile_scheduler.rb
+++ app/workers/profile_scheduler.rb
@@ -4,11 +4,7 @@ class ProfileScheduler
   def self.perform(profile_id)
     profile = Profile.find(profile_id)
     unless profile.scheduled?
-<<<<<<< HEAD
-      scheduler = Scheduler.scheduling_profile(profile_id).to_a.max{ |x,y| x.required_samples(profile_id)<=>y.required_samples(profile_id) }
-=======
       scheduler = profile.schedulers.with_max_samples
->>>>>>> mongoid_upgrade
       scheduler.schedule_profile(profile) if scheduler
     end
   end
