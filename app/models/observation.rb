d4dd4d3d1570e662f8c0503deff37554453d0719
bcassell <bcassell@umich.edu>
2012-09-02 22:18:18 -0400

    defaults

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -3,7 +3,7 @@ class Observation
 
   embedded_in :profiles
   embeds_many :symmetry_groups, as: :role_strategy_partitionable
-  field :features, type: Hash
+  field :features, type: Hash, default: {}
 
   accepts_nested_attributes_for :symmetry_groups
 end
