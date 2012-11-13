b994d0d1b32d6159cccb671565e9d78227308519
Ben-Alexander Cassell <bcassell@umich.edu>
2012-11-13 10:56:58 -0500

    flexible profile requests

--- app/controllers/api/v3/profiles_controller.rb
+++ app/controllers/api/v3/profiles_controller.rb
@@ -1,7 +1,7 @@
 class Api::V3::ProfilesController < Api::V3::BaseController
   def show
     begin
-      render json: ProfilePresenter.new(Profile.find(params[:id])).to_json, status: 200
+      render json: ProfilePresenter.new(Profile.find(params[:id])).to_json(granularity: params[:granularity]), status: 200
     rescue
       render json: {error: "the profile you were looking for could not be found"}.to_json, status: 404
     end
