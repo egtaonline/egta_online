a70be497c016b17024d83bbb192a68cc12869ae9
Ben-Alexander Cassell <bcassell@umich.edu>
2013-02-01 14:16:10 -0500

    pbs script should now do permissions correctly

--- lib/backend/flux/pbs_wrapper.rb
+++ lib/backend/flux/pbs_wrapper.rb
@@ -18,17 +18,18 @@ class PbsWrapper
 #PBS -q #{queue}
 #PBS -l nodes=#{simulation.scheduler_nodes},pmem=#{scheduler.process_memory}mb,walltime=#{pbs_wall_time},qos=#{queue}
 #PBS -N egta-#{simulator.name.downcase.gsub(' ', '_')}
+#PBS -W umask=0007
+#PBS -W group_list=wellmangroup
 #PBS -o #{sim_path}/#{simulation.id}/out
 #PBS -e #{sim_path}/#{simulation.id}/error
 #PBS -M #{simulator.email}
-
+umask 0007
 mkdir /tmp/${PBS_JOBID}
 cp -r #{root_path}/* /tmp/${PBS_JOBID}
 cp -r #{sim_path}/#{simulation.id} /tmp/${PBS_JOBID}
 cd /tmp/${PBS_JOBID}
 script/batch #{simulation.id} #{simulation.size}#{extra_args}
 cp -r /tmp/${PBS_JOBID}/#{simulation.id} #{sim_path}
-chmod -R ug+rw #{sim_path}/#{simulation.id}
 rm -rf /tmp/${PBS_JOBID}
 HEREDOC
 
--- spec/lib/backend/flux/pbs_wrapper_spec.rb
+++ spec/lib/backend/flux/pbs_wrapper_spec.rb
@@ -17,17 +17,18 @@ describe PbsWrapper do
 #PBS -q flux
 #PBS -l nodes=1,pmem=1000mb,walltime=02:30:00,qos=flux
 #PBS -N egta-fake
+#PBS -W umask=0007
+#PBS -W group_list=wellmangroup
 #PBS -o #{Yetting.deploy_path}/simulations/3/out
 #PBS -e #{Yetting.deploy_path}/simulations/3/error
 #PBS -M test@test.com
-
+umask 0007
 mkdir /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/fake-totally/fake/* /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/simulations/3 /tmp/${PBS_JOBID}
 cd /tmp/${PBS_JOBID}
 script/batch 3 30
 cp -r /tmp/${PBS_JOBID}/3 #{Yetting.deploy_path}/simulations
-chmod -R ug+rw #{Yetting.deploy_path}/simulations/3
 rm -rf /tmp/${PBS_JOBID}
 HEREDOC
         f = double("file")
@@ -51,17 +52,18 @@ HEREDOC
 #PBS -q cac
 #PBS -l nodes=1,pmem=1000mb,walltime=02:30:00,qos=cac
 #PBS -N egta-fake
+#PBS -W umask=0007
+#PBS -W group_list=wellmangroup
 #PBS -o #{Yetting.deploy_path}/simulations/3/out
 #PBS -e #{Yetting.deploy_path}/simulations/3/error
 #PBS -M test@test.com
-
+umask 0007
 mkdir /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/fake-totally/fake/* /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/simulations/3 /tmp/${PBS_JOBID}
 cd /tmp/${PBS_JOBID}
 script/batch 3 30
 cp -r /tmp/${PBS_JOBID}/3 #{Yetting.deploy_path}/simulations
-chmod -R ug+rw #{Yetting.deploy_path}/simulations/3
 rm -rf /tmp/${PBS_JOBID}
 HEREDOC
         f = double("file")
@@ -85,17 +87,18 @@ HEREDOC
 #PBS -q cac
 #PBS -l nodes=2,pmem=1000mb,walltime=02:30:00,qos=cac
 #PBS -N egta-fake
+#PBS -W umask=0007
+#PBS -W group_list=wellmangroup
 #PBS -o #{Yetting.deploy_path}/simulations/3/out
 #PBS -e #{Yetting.deploy_path}/simulations/3/error
 #PBS -M test@test.com
-
+umask 0007
 mkdir /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/fake-totally/fake/* /tmp/${PBS_JOBID}
 cp -r #{Yetting.deploy_path}/simulations/3 /tmp/${PBS_JOBID}
 cd /tmp/${PBS_JOBID}
 script/batch 3 30 ${PBS_NODEFILE}
 cp -r /tmp/${PBS_JOBID}/3 #{Yetting.deploy_path}/simulations
-chmod -R ug+rw #{Yetting.deploy_path}/simulations/3
 rm -rf /tmp/${PBS_JOBID}
 HEREDOC
         f = double("file")
