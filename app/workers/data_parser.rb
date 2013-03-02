5e4f767557d1cb303303c42bedc88f972f8714be
Ben-Alexander Cassell <bcassell@umich.edu>
2013-03-01 19:00:28 -0500

    fixing path bugs

--- app/workers/data_parser.rb
+++ app/workers/data_parser.rb
@@ -2,7 +2,7 @@ class DataParser
   include Sidekiq::Worker
   sidekiq_options unique: true, queue: 'high_concurrency'
 
-  def perform(number, location="#{Rails.root}/tmp/data/#{number}")
+  def perform(number, location="#{Backend.backend_implementation.simulations_path}/#{number}")
     simulation = Simulation.find(number)
     if simulation.state != 'complete'
       files = Dir.entries(location).keep_if{ |name| name =~ /\A(.*)observation(.)*.json\z/ }
