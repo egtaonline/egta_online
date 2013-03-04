8844f78160adca75d17aacb95c835b540e03387f
Ben-Alexander Cassell <bcassell@umich.edu>
2013-03-04 15:54:46 -0500

    refactoring api

--- app/controllers/api/v3/games_controller.rb
+++ app/controllers/api/v3/games_controller.rb
@@ -1,56 +1,7 @@
-class Api::V3::GamesController < Api::V3::BaseController
-  before_filter :find_game, only: [:show, :add_role, :add_strategy, :remove_role, :remove_strategy]
-  before_filter :validate_role, only: [:add_role, :add_strategy, :remove_role, :remove_strategy]
-  before_filter :validate_strategy, only: [:add_strategy, :remove_strategy]
-  before_filter :validate_role_count, only: [:add_role]
-
-  include Api::V3::RoleManipulator
-
-  def add_role
-    if @game.roles.where(name: params[:role]).count > 0
-      respond_with(@game, status: 304)
-    else
-      @game.add_role(params[:role], params[:count].to_i)
-      respond_with(@game)
-    end
-  end
-
-  def add_strategy
-    if @game.roles.where(name: params[:role]).count == 0
-      if params[:count] == nil || params[:count] == "" || params[:count].to_i == 0
-        respond_with({error: "you did not specify a count for this role"}, status: 422, location: nil)
-      else
-        @game.add_role(params[:role], params[:count].to_i)
-        @game.reload
-      end
-    end
-    if @game.roles.where(name: params[:role]).first.strategies.include?(params[:strategy]) == false
-      @game.add_strategy(params[:role], params[:strategy])
-      respond_with(@game)
-    else
-      respond_with(@game, status: 304)
-    end
-  end
-
-  def remove_role
-    if @game.roles.where(name: params[:role]).count == 0
-      respond_with({message: "the role did not exist"}, status: 204, location: nil)
-    else
-      @game.remove_role(params[:role])
-      respond_with(@game, status: 202)
-    end
-  end
-
-  def remove_strategy
-    if @game.roles.where(name: params[:role]).count == 0
-      respond_with({error: "the role did not exist"}, status: 404, location: nil)
-    elsif @game.roles.where(name: params[:role]).first.strategies.include?(params[:strategy]) == false
-      respond_with({message: "the role did not exist"}, status: 204, location: nil)
-    else
-      @game.remove_strategy(params[:role], params[:strategy])
-      respond_with(@game)
-    end
-  end
+class Api::V3::GamesController < Api::V3::RoleManipulator
+  before_filter :find_game, only: :show
+  before_filter :validate_role_count, only: :add_role
+  before_filter :add_role_for_strategy, only: :add_strategy
 
   def index
     render json: "{\"games\":#{Game.collection.find.select(name: 1, simulator_fullname: 1, configuration: 1, size: 1).to_json}}", status: 200
@@ -73,6 +24,13 @@ class Api::V3::GamesController < Api::V3::BaseController
       @game = Game.find(params[:id])
     rescue
       render json: {error: "the game you were looking for could not be found"}.to_json, status: 404
+    end
+  end
+
+  def add_role_for_strategy
+    if @subject.roles.where(name: params[:role]).count == 0
+      @subject.add_role(params[:role], params[:count].to_i)
+      @subject.reload
     end
   end
 end
--- app/controllers/api/v3/simulators_controller.rb
+++ app/controllers/api/v3/simulators_controller.rb
@@ -1,10 +1,4 @@
-class Api::V3::SimulatorsController < Api::V3::BaseController
-  before_filter :find_simulator, only: [:add_role, :add_strategy, :remove_role, :remove_strategy]
-  before_filter :validate_role, only: [:add_role, :add_strategy, :remove_role, :remove_strategy]
-  before_filter :validate_strategy, only: [:add_strategy, :remove_strategy]
-
-  include Api::V3::RoleManipulator
-
+class Api::V3::SimulatorsController < Api::V3::RoleManipulator
   def index
     render json: "{\"simulators\":#{Simulator.collection.find.select(name: 1, version: 1).to_json}}", status: 200
   end
@@ -12,54 +6,6 @@ class Api::V3::SimulatorsController < Api::V3::BaseController
   def show
     begin
       render json: Simulator.collection.find(_id: Moped::BSON::simulatorId(params[:id])).select(name: 1, version: 1, configuration: 1, 'roles.name' => 1, 'roles.strategies' => 1).to_json, status: 200
-    rescue
-      render json: {error: "the simulator you were looking for could not be found"}.to_json, status: 404
-    end
-  end
-
-  def add_role
-    if @simulator.roles.where(name: params[:role]).count > 0
-      respond_with(@simulator, status: 304)
-    else
-      @simulator.add_role(params[:role])
-      respond_with(@simulator)
-    end
-  end
-
-  def add_strategy
-    if @simulator.roles.where(name: params[:role]).count == 0 || @simulator.roles.where(name: params[:role]).first.strategies.include?(params[:strategy]) == false
-      @simulator.add_strategy(params[:role], params[:strategy])
-      respond_with(@simulator)
-    else
-      respond_with(@simulator, status: 304)
-    end
-  end
-
-  def remove_role
-    if @simulator.roles.where(name: params[:role]).count == 0
-      respond_with({message: "the role did not exist"}, status: 204, location: nil)
-    else
-      @simulator.remove_role(params[:role])
-      respond_with(@simulator, status: 202)
-    end
-  end
-
-  def remove_strategy
-    if @simulator.roles.where(name: params[:role]).count == 0
-      respond_with({error: "the role did not exist"}, status: 404, location: nil)
-    elsif @simulator.roles.where(name: params[:role]).first.strategies.include?(params[:strategy]) == false
-      respond_with({message: "the role did not exist"}, status: 204, location: nil)
-    else
-      @simulator.remove_strategy(params[:role], params[:strategy])
-      respond_with(@simulator)
-    end
-  end
-
-  private
-
-  def find_simulator
-    begin
-      @simulator = Simulator.find(params[:id])
     rescue
       render json: {error: "the simulator you were looking for could not be found"}.to_json, status: 404
     end
