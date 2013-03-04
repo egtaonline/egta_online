4c3dc1d92d6e9ff5babf5cbb9fa205064db7ccf8
Ben-Alexander Cassell <bcassell@umich.edu>
2013-03-04 14:50:56 -0500

    improved page loads by restricting when data is loaded

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,4 @@
 class ProfilesController < ApplicationController
   respond_to :html
-  expose(:profile)
+  expose(:profile){ Profile.where(_id: params[:id]).only(:configuration, :simulator_id, :assignment, :sample_count, :symmetry_groups).first }
 end
