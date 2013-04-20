9e69645955809bc4298069822b0b8aad16a48842
Ben-Alexander Cassell <bcassell@umich.edu>
2013-04-20 15:10:50 -0400

    further attempts to limit queueing

--- app/workers/simulation_queuer.rb
+++ app/workers/simulation_queuer.rb
@@ -5,6 +5,10 @@ class SimulationQueuer
   def perform
     prep_service = SimulationPrepService.new
     to_be_queued = Simulation.queueable.to_a
+    if to_be_queued.size > Simulation.simulation_limit
+      to_be_queued = to_be_queued[0..Simulation.simulation_limit]
+    end
+    puts to_be_queued.size
     to_be_queued.each{ |simulation| prep_service.prepare_simulation(simulation) }
     sleep 3
     to_be_queued.each{ |simulation| Backend.schedule_simulation(simulation) }
