1da8bd117de465f1e28e4c2eb1799bbe258c8069
Ben-Alexander Cassell <bcassell@umich.edu>
2013-05-17 17:49:07 -0400

    log timing

--- app/workers/simulation_queuer.rb
+++ app/workers/simulation_queuer.rb
@@ -9,8 +9,11 @@ class SimulationQueuer
       to_be_queued = to_be_queued[0..Simulation.simulation_limit]
     end
     puts to_be_queued.size
+    puts Time.now
     to_be_queued.each{ |simulation| prep_service.prepare_simulation(simulation) }
-    sleep 3
+    puts Time.now
+    sleep 1
     to_be_queued.each{ |simulation| Backend.schedule_simulation(simulation) }
+    puts Time.now
   end
 end
