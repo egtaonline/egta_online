f93786e9dae6187dd9edc5757e9ca1b6235cdd83
Ben-Alexander Cassell <bcassell@umich.edu>
2013-05-17 18:09:49 -0400

    more logging

--- app/workers/profile_scheduler.rb
+++ app/workers/profile_scheduler.rb
@@ -3,10 +3,19 @@ class ProfileScheduler
   sidekiq_options queue: 'high_concurrency'
 
   def perform(profile_id)
+    puts Time.now
+    puts 'Getting profile'
     profile = Profile.where(_id: profile_id).without(:symmetry_groups, :observations).first
+    puts Time.now
+    puts 'Got Profile'
     unless profile.scheduled?
+      puts Time.now
+      puts 'Picking a scheduler'
       scheduler = profile.schedulers.with_max_samples
+      puts Time.now
+      puts 'Scheduling'
       scheduler.schedule_profile(profile) if scheduler
+      puts Time.now
     end
   end
 end
